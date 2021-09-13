4b3854e8d1741d19420de806e99e3192be9c6e2b
Ananda Wiradharma <anandawira754@gmail.com>
2021-09-13 17:45:42 +0700

    add redis config

---
+++ configs/redis-client.js
@@ -0,0 +1,13 @@
+const redis = require('redis')
+const {promisify} = require('util')
+
+const client = redis.createClient(process.env.REDIS_URL)
+
+client.on('error', (error)=>{
+  console.error(error)
+})
+
+const getAsync = promisify(client.get).bind(client);
+const setAsync = promisify(client.set).bind(client);
+
+module.exports = {client, getAsync, setAsync}
