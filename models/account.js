39acc014dd98a8ab2bb7bfeb6f965f7f9647f340
Ananda Wiradharma <anandawira754@gmail.com>
2021-09-14 15:41:22 +0700

    change isApproved to status

--- models/account.js
+++ models/account.js
@@ -13,7 +13,11 @@ const accountSchema = new Schema(
     },
     password: { type: String, required: true },
     isAdmin: { type: Boolean, default: false },
-    isApproved: { type: Boolean, default: false },
+    status: {
+      type: String,
+      enum: ['pending', 'approved', 'rejected'],
+      default: 'pending',
+    },
   },
   { timestamps: true }
 );
