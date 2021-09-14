a80ca8e68537f8d72e62253db13e80b73e3641bb
Zidni Imani <zidni.imani@gmail.com>
2021-09-14 19:29:11 +0700

    forgot password

--- controllers/accountsController.js
+++ controllers/accountsController.js
@@ -2,105 +2,110 @@ const Account = require('../models/account');
 const bcrypt = require('bcryptjs');
 const { body, validationResult } = require('express-validator');
 const nodemailer = require('nodemailer');
+const jwt = require('jsonwebtoken');
 
-exports.register_new_account = [
-  // Validate and sanitize fields.
-  body('first_name')
-    .trim()
-    .isLength({ min: 1 })
-    .withMessage('First name must be specified.')
-    .isAlpha()
-    .withMessage('First name has non-alphabetical characters.'),
-  body('last_name')
-    .trim()
-    .isLength({ min: 1 })
-    .withMessage('Last name must be specified.')
-    .isAlpha()
-    .withMessage('Last name has non-alphabetical characters.'),
-  body('email')
-    .trim()
-    .isEmail()
-    .withMessage('Email invalid.')
-    .toLowerCase()
-    .custom(async (email) => {
-      const account = await Account.isEmailExist(email);
-      if (account) {
-        return Promise.reject('Email already in use.');
+// Register
+exports.register_new_account = async(req, res) => {
+  try {
+    // Sanitizing and validating
+    // ???
+
+    // Hashing password
+    const hashedpassword = await bcrypt.hash(req.body.password, 10);
+
+    // New Account
+    const account = new Account({
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      email: req.body.email,
+      password: hashedpassword
+    });
+
+    // Save
+    const saveAccount = await account.save();
+
+    // Send status success
+    res.sendStatus(201);
+    // res.status(201).json(saveAccount); //delete later
+
+    // Send email to Admin 
+    let transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'glintsipe1@gmail.com', // generated ethereal user
+        pass: process.env.GMAIL_PASSWORD, // generated ethereal password
+      },
+    });
+    transporter.sendMail(
+      {
+        from: '"Attendance App Glints-IPE1" <glintsipe1@gmail.com>', // sender address
+        to: 'zidni.imani@gmail.com', // list of receivers change/delete later
+        subject: 'Account Register Approval', // Subject line
+        html: `<p>Hello Admin,</p>
+        <p>An account has just registered and needs your approval to login.</p>
+        <p>Please check your dashboard page and make sure to approve the right employee's email.</p>
+        <p><a href="https://attendance.app/dashboard">Click here to check it now.</a></p>
+        <p></p>
+        <p>Attendance App - Glints IPE 1</p>
+        `,
       }
-    }),
-  body('password')
-    .isLength({ min: 8 })
-    .withMessage('Password must be more than 8 character length.'),
+    );
+  } catch (error){
+    res.status(400).json({message: error.message});
+  }
+}
 
-  (req, res, next) => {
-    // Check validation result
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
+// Forgot Password
+exports.send_reset_password_email = async(req, res) => {
+  // Check email exis
+  const account = await Account.isEmailExist(req.body.email);
+  if(!account) return res.status(404).json({message:"Email not found."});
 
-    // Hash user's password using bcrypt
-    bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
-      // Check if hashing failed
-      if (err) {
-        return res.status(500).send('Password hashing failed.');
-      }
 
-      // Create new User object from request body and hashed password
-      const account = new Account({
-        first_name: req.body.first_name,
-        last_name: req.body.last_name,
-        email: req.body.email,
-        password: hashedPassword,
-      });
+  try {
+    const resetToken = jwt.sign(
+      { id: account.id },
+      process.env.FORGET_PASSWORD_SECRET,
+      { expiresIn: '15m' },
+    );
 
-      account.save((err) => {
-        // Check if database query failed and put error message to response
-        if (err) {
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'glintsipe1@gmail.com', // generated ethereal user
+        pass: process.env.GMAIL_PASSWORD, // generated ethereal password
+      },
+    });
+
+    // send mail with defined transport object
+    transporter.sendMail(
+      {
+        from: '"Attendance App Glints-IPE1" <glintsipe1@gmail.com>', // sender address
+        to: req.body.email, // list of receivers
+        subject: 'Password Reset', // Subject line
+        html: `<p>Hey ${account.first_name}!</p>
+        <p>Looks like you forgot your password. We cannot simply send you your old password.</p>
+        <p>A unique link to reset your password has been generated for you. To
+        reset your password, click the following link and follow the instructions.</p>
+        <p><a href="https://${req.hostname}/reset-password/${resetToken}">Click here to reset your password</a>. This link will expire in 15 minutes.</p>
+        <p></p>
+        <p>Attendance App - Glints IPE 1</p>
+        `, // html body
+      },
+      (error, info) => {
+        // Check errors
+        if (error) {
           return next(err);
         }
-        // Send response 'Created' on success
-        return res.sendStatus(201);
-      });
-
-      // create reusable transporter object using the default SMTP transport
-      let transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-          user: 'glintsipe1@gmail.com', // generated ethereal user
-          pass: process.env.GMAIL_PASSWORD, // generated ethereal password
-        },
-      });
-
-      // send mail to admin
-      transporter.sendMail(
-        {
-          from: '"Attendance App Glints-IPE1" <glintsipe1@gmail.com>', // sender address
-          to: 'zidni.imani@gmail.com', // list of receivers
-          subject: 'Account Register Approval', // Subject line
-          html: `<p>Hello admin,</p>
-          <p>
-            An account has just registered and needs your approval to continue log in.
-            Please check your dashboard page and make sure to approve the right employee's email.
-            <a href="https://attendance.app/dashboard">Click here to check it now.</a>
-          </p>
-          <p></p>
-          <p>Attendance App - Glints IPE 1</p>
-          `, // html body
-        },
-        (err, info) => {
-          // Check errors
-          if (err) {
-            return next(err);
-          }
-          // Send response
-          return res.sendStatus(200);
-        },
-      );
-    });
-  },
-];
-
+        // Send response
+        return res.sendStatus(200);
+      },
+    );
+  } catch (error){
+    res.status(400).json({message: error.message});
+  }
+}
 exports.login = (req, res) => {
   return res.send("Login");
 };
