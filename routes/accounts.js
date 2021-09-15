98f998068cd8e0a676a30c1f5f1bb3fc9402337d
Zidni Imani <zidni.imani@gmail.com>
2021-09-15 11:52:55 +0700

    update swagger

--- routes/accounts.js
+++ routes/accounts.js
@@ -2,18 +2,8 @@ var express = require('express');
 var router = express.Router();
 const rateLimit = require('express-rate-limit');
 
-<<<<<<< HEAD
-const accountsController = require('../controllers/accountsController');
-
-/* 
-[POST] /v1/accounts
-Register a new account to the system 
-*/
-router.post('/', accountsController.register_new_account);
-=======
 const accountsController = require("../controllers/accountsController");
 const forgetLimit = rateLimit({ windowMs: 1 * 60 * 60 * 1000, max: 5 });
-
 
 // [POST] /v1/accounts/register
 // Register a new account to the system 
@@ -30,7 +20,6 @@ router.post(
 // [POST] /v1/accounts/reset-password
 // Reset user's password with token from server 
 router.post("/reset-password/:resetToken", accountsController.reset_password);
->>>>>>> origin/zidni
 
 // [POST] /v1/accounts/auth
 // Login to the system, get user information, access_token, and refresh_token
