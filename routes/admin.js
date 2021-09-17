ce142a1f1841547e1da2b84458e583a0d21fc4fb
Zidni Imani <zidni.imani@gmail.com>
2021-09-17 10:55:37 +0700

    Add information for admin controller

--- routes/admin.js
+++ routes/admin.js
@@ -5,8 +5,16 @@ const authenticateToken = require('../middleware/authenticateAccessToken');
 
 router.use(authenticateToken);
 
+/* 
+[GET] /v1/admin
+Get list account for approval
+*/
 router.get("/", adminController.list_approval_account);
 
+/* 
+[PUT] /v1/admin/{accountId}
+Update account approval
+*/
 router.put("/:id?", adminController.update_approval);
 
 module.exports = router;
